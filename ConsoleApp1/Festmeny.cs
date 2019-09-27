64ced3e8bc02240ff9317bd91599db337620de6d
Vorák Gergő <vorakgeri@gmail.com>
2019-09-27 10:58:35 +0200

    elsoFeladat

--- ConsoleApp1/Festmeny.cs
+++ ConsoleApp1/Festmeny.cs
@@ -9,23 +9,63 @@ namespace ConsoleApp1
     class Festmeny
     {
         string cim;
-        string festo { get; }
-        string stilus { get; }
-        int licitekSzama { get; }
-        int legmagasabbLicit { get; }
-        DateTime legmagasabbLicitIdeje { get; set; }
-        bool elkelt { get; set; }
+        string festo;
+        public string Festo { get { return festo; } }
+        string stilus;
+        public string Stilus { get { return stilus; } }
+        int licitekSzama;
+        public int LicitekSzama { get { return licitekSzama; } }
+        int legmagasabbLicit;
+        public int LegmagasabbLicit { get { return legmagasabbLicit; } }
+        DateTime legmagasabbLicitIdeje;
+        public DateTime LegmagasabbLicitIdeje
+        {
+            get { return legmagasabbLicitIdeje; }
+            set { legmagasabbLicitIdeje = value; }
+        }
+        bool elkelt;
+        public bool Elkelt {
+            get { return elkelt; }
+            set { elkelt = value; }
+        }
 
-        /*+Festmeny(cím : string, festo : string, stilus : string) 
-         * +Festo {get;} : string +Stilus {get;} : string +LicitekSzama {get;} : int +LegmagasabbLicit {get;} : int 
-         * +LegutolsoLicitIdeje {get;} : DateTime +Elkelt {get; set;} : bool +Licit() : void +Licit(mertek : int) : void*/
+        int kezdetiLicit = 100;
+
         public Festmeny(string cim, string festo, string stilus)
         {
             this.cim = cim;
             this.festo = festo;
             this.stilus = stilus;
+            this.licitekSzama = 0;
+            this.legmagasabbLicit = 0;
+            elkelt = false;
         }
+        public void Licit()
+        {
+            if (elkelt==true)
+            {
+                Console.WriteLine("Hiba! A festmény elkelt!");
+            }
+            else if (licitekSzama==0)
+            {
+                legmagasabbLicit = kezdetiLicit;
+                licitekSzama++;
+                legmagasabbLicitIdeje = DateTime.Now;// állítsa be a legutolsó licit idejét az aktuális időpontra.
 
+            }
+            else if (licitekSzama > 0)
+            {
+                legmagasabbLicit=(legmagasabbLicit/10)*11;
+                licitekSzama++;
+                legmagasabbLicitIdeje = DateTime.Now;// állítsa be a legutolsó licit idejét az aktuális időpontra.
+            }
+        }
+        public void Licit(int mertek)
+        {
+            legmagasabbLicit = (legmagasabbLicit / 10) * 11;
+            licitekSzama++;
+            legmagasabbLicitIdeje = DateTime.Now;
+        }
 
     }
 }
